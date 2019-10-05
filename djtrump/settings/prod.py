730853cb3a67727ade30b200e37cc308a6ff1e36
Md. Ruhul Amin Sujon <sujonict07@gmail.com>
2019-10-05 08:54:17 +0600

    Update prod.py

--- djtrump/settings/prod.py
+++ djtrump/settings/prod.py
@@ -1,11 +1,17 @@
 from .base import *
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'djtrumpprod',
+#        'USER': 'djtrumpuser',
+#        'PASSWORD': 'djtrump',
+#        'PORT': '5432',
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djtrumpprod',
-        'USER': 'djtrumpuser',
-        'PASSWORD': 'djtrump',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
