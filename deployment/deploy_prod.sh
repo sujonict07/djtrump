5aadde6c412f22376e2a567d603dd0e69112da1f
Md. Ruhul Amin Sujon <sujonict07@gmail.com>
2019-10-05 09:18:15 +0600

    Update deploy_prod.sh

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -3,7 +3,7 @@
 sshpass -p jen ssh -o StrictHostKeyChecking=no jen@192.168.100.249 /bin/bash <<EOF
   cd djtrump
   git pull
-  source /home/jen/env/bin/pip/activate
+  source /home/jen/env/bin/activate
   pip install -r requirements.txt
   ./manage.py migrate
   sudo supervisorctl restart djtrump
