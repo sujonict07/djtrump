e22e918ce3498a58bd403e8b4efe6d18c16608af
Md. Ruhul Amin Sujon <sujonict07@gmail.com>
2019-10-05 09:15:28 +0600

    Update deploy_prod.sh

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ssh -o StrictHostKeyChecking=no jen@192.168.100.249 /bin/bash <<EOF
+sshpass -p jen ssh -o StrictHostKeyChecking=no jen@192.168.100.249 /bin/bash <<EOF
   cd djtrump
   git pull
   source /home/jen/env/bin/pip/activate
